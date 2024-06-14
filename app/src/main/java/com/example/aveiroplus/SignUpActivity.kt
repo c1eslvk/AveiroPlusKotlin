f33706c2414f5e51fa483bb8f72b342b954039a2
maksc <maks.cieslak13@gmail.com>
2024-06-14 17:52:30 +0100

    LoginDesign

--- app/src/main/java/com/example/aveiroplus/SignUpActivity.kt
+++ app/src/main/java/com/example/aveiroplus/SignUpActivity.kt
@@ -1,23 +1,38 @@
 package com.example.aveiroplus
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import com.example.aveiroplus.ui.theme.AveiroPlusTheme
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : ComponentActivity() {
 
@@ -62,94 +77,137 @@ fun SignUpScreen(
     var pass by remember { mutableStateOf("") }
     var confirmPass by remember { mutableStateOf("") }
     val context = LocalContext.current
+    val isDarkTheme = isSystemInDarkTheme()
+    val logoResource = if (isDarkTheme) R.drawable.logodark else R.drawable.logo
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(horizontal = 32.dp, vertical = 16.dp)
     ) {
-        TextField(
+        Spacer(modifier = Modifier.height(24.dp))
+        Image(
+            painter = painterResource(id = logoResource),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .height(80.dp)
+                .width(200.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = "Register to start!",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        OutlinedTextField(
             value = name,
             onValueChange = { name = it },
-            label = { Text("Name") }
+            label = { Text("Name") },
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
         )
         Spacer(modifier = Modifier.height(8.dp))
-        TextField(
+        OutlinedTextField(
             value = surname,
             onValueChange = { surname = it },
-            label = { Text("Surname") }
+            label = { Text("Surname") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
         )
         Spacer(modifier = Modifier.height(8.dp))
-        TextField(
+        OutlinedTextField(
             value = email,
             onValueChange = { email = it },
-            label = { Text("Email") }
+            label = { Text("Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
         )
         Spacer(modifier = Modifier.height(8.dp))
-        TextField(
+        OutlinedTextField(
             value = pass,
             onValueChange = { pass = it },
             label = { Text("Password") },
-            visualTransformation = PasswordVisualTransformation()
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
         )
         Spacer(modifier = Modifier.height(8.dp))
-        TextField(
+        OutlinedTextField(
             value = confirmPass,
             onValueChange = { confirmPass = it },
             label = { Text("Confirm Password") },
-            visualTransformation = PasswordVisualTransformation()
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            if (name.isNotEmpty() && surname.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
-                if (pass == confirmPass) {
-                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            val userId = firebaseAuth.currentUser?.uid
-                            val user = hashMapOf(
-                                "uid" to userId,
-                                "name" to name,
-                                "surname" to surname,
-                                "email" to email,
-                                "role" to "USER",
-                                "profileImageUrl" to "",
-                                "registeredEventIds" to emptyList<String>()
-                            )
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(
+            onClick = {
+                if (name.isNotEmpty() && surname.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                    if (pass == confirmPass) {
+                        firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                val userId = firebaseAuth.currentUser?.uid
+                                val user = hashMapOf(
+                                    "uid" to userId,
+                                    "name" to name,
+                                    "surname" to surname,
+                                    "email" to email,
+                                    "role" to "USER",
+                                    "profileImageUrl" to "",
+                                    "registeredEventIds" to emptyList<String>()
+                                )
 
-                            if (userId != null) {
-                                fireStore.collection("users").document(userId).set(user)
-                                    .addOnSuccessListener {
-                                        onSignUpSuccess()
-                                    }
-                                    .addOnFailureListener { e ->
-                                        Toast.makeText(
-                                            context,
-                                            e.message,
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-                                    }
+                                if (userId != null) {
+                                    fireStore.collection("users").document(userId).set(user)
+                                        .addOnSuccessListener {
+                                            onSignUpSuccess()
+                                        }
+                                        .addOnFailureListener { e ->
+                                            Toast.makeText(
+                                                context,
+                                                e.message,
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                }
+                            } else {
+                                Toast.makeText(
+                                    context,
+                                    it.exception?.message,
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
-                        } else {
-                            Toast.makeText(
-                                context,
-                                it.exception?.message,
-                                Toast.LENGTH_SHORT
-                            ).show()
                         }
+                    } else {
+                        Toast.makeText(context, "Password is not matching", Toast.LENGTH_SHORT).show()
                     }
                 } else {
-                    Toast.makeText(context, "Password is not matching", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
                 }
-            } else {
-                Toast.makeText(context, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-            }
-        }) {
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
             Text("Sign Up")
         }
-        Spacer(modifier = Modifier.height(8.dp))
-        TextButton(onClick = onSignInClicked) {
+        Spacer(modifier = Modifier.height(16.dp))
+        TextButton(
+            onClick = onSignInClicked,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        ) {
             Text("Already have an account? Sign In")
         }
     }

24c887293de11302c5e679e132f4ee51e005c5ac
Mateusz Wyciszkiewicz <matiwycisz@gmail.com>
2024-06-16 23:00:40 +0200

    map marker basics

---
+++ app/src/main/java/com/example/aveiroplus/components/MapMarker.kt
@@ -0,0 +1,8 @@
+package com.example.aveiroplus.components;
+
+data class MapMarker(
+    val markerId: String = "",
+    val lat: Double = 0.0,
+    val long: Double = 0.0,
+    val relatedUser: UserProfile,
+) {}

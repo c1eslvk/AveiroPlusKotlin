b84b0e2670d573b2fd80856c059fd79f3e6044ab
Antoni <a.kobylecki.02@gmail.com>
2024-06-19 10:18:21 +0100

    add price for event

--- app/src/main/java/com/example/aveiroplus/EventDetailsScreen.kt
+++ app/src/main/java/com/example/aveiroplus/EventDetailsScreen.kt
@@ -141,6 +141,12 @@ fun EventDetailsScreen(navController: NavController, eventId: String) {
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
+                text = "Price: ${event?.price} EUR",  // Displaying price here
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
                 text = event?.description ?: "",
                 style = MaterialTheme.typography.bodyMedium
             )
